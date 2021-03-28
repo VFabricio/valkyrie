d7b77c9f400dd1be78d74bef2b26bccdbbf60242
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2021-03-27 21:21:25 -0300

    chore(npm): rename package to valkyrie-ts

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import typescript from 'rollup-plugin-typescript2'
 export default {
   input: 'src/index.ts',
   output: [
-    { file: 'dist/index.cjs', name: 'valkyrie', format: 'cjs', sourcemap: true },
+    { file: 'dist/index.cjs', name: 'valkyrie-ts', format: 'cjs', sourcemap: true },
     { file: 'dist/index.esm.js', format: 'es', sourcemap: true },
   ],
   plugins: [
